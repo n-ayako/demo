5030fd7afdcd2f7ec17b1529279b26afe27ccc8b
n-ayako <ayako.nakano@fonfun.co.jp>
2024-05-17 11:54:19 +0900

    タイムリーフ動作確認

---
+++ src/main/java/com/example/demo/controller/controller.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class controller {
+	
+	@GetMapping("/index")
+	public String example(Model model) {
+		
+	    model.addAttribute("message", "Hello World");
+	    
+	    return "index";
+	}
+	
+}
+
